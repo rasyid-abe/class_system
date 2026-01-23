af5bb266234c212d76739f96800485c17a70c7da
rasyid-abe <razyid72@gmail.com>
2026-01-23 22:17:50 +0700

    minor change

--- app/Views/templates/sidemenu.php
+++ app/Views/templates/sidemenu.php
@@ -63,8 +63,6 @@
 </style>
 
 <div class="tab-content">
-    <span class="badge badge-light-info text-center"><?= datenow() ?></span>
-    <span class="badge badge-light-info text-center"><?= datetimenow() ?></span>
     <?php if (in_array(11, session()->get('c_role'))): ?>
         <div class="tab-pane fade <?= $page == "Dashboard" ? 'active show' : '' ?>" id="home_dashboard" role="tabpanel">
             <div class="mx-5">
